1092d30ba575b8843fb3e647f42f4bddecd8b07f
Jan Kåre Vatne <jkvatne@online.no>
2021-11-07 20:27:40 +0100

    WIP Resize

--- wid/resizer.go
+++ wid/resizer.go
@@ -40,12 +40,49 @@ func Split(th *Theme, Axis layout.Axis, w1 layout.Widget, w2 layout.Widget) func
 // The widgets w1 and w2 must be able to gracefully resize their minimum and maximum dimensions
 // in order for the resize to be smooth.
 func (rs *Resize) Layout(gtx C, th *Theme, w1 layout.Widget, w2 layout.Widget) D {
-	var dims D
 	// Compute the first widget's max width/height.
 	m := op.Record(gtx.Ops)
 	rs.Length = gtx.Constraints.Max.X
 	rs.Pos = int(rs.Ratio * float32(rs.Length))
-	dims = rs.layoutSash(gtx, rs.Axis)
+	gtx.Constraints.Min = image.Point{}
+	dims := gtx.Constraints.Max
+	dims.X = 12
+	size := image.Pt(12, dims.Y)
+	m1 := clip.Rect{Max: size}.Push(gtx.Ops)
+	paint.ColorOp{Color: RGB(0x777777)}.Add(gtx.Ops)
+	paint.PaintOp{}.Add(gtx.Ops)
+	var de *pointer.Event
+	for _, e := range rs.drag.Events(gtx.Metric, gtx, gesture.Axis(rs.Axis)) {
+		if e.Type == pointer.Drag {
+			de = &e
+		}
+	}
+	if de != nil {
+		xy := de.Position.X
+		if rs.Axis == layout.Vertical {
+			xy = de.Position.Y
+		}
+		rs.Pos += int(xy)
+	}
+
+	// Clamp the handle position, leaving it always visible.
+	if rs.Pos < 0 {
+		rs.Pos = 0
+	} else if rs.Pos > rs.Length {
+		rs.Pos = rs.Length
+	}
+
+	rect := image.Rectangle{Max: dims}
+	m2 := pointer.Rect(rect).Push(gtx.Ops)
+	rs.drag.Add(gtx.Ops)
+
+	if rs.Axis == layout.Horizontal {
+		pointer.CursorNameOp{Name: pointer.CursorColResize}.Add(gtx.Ops)
+	} else {
+		pointer.CursorNameOp{Name: pointer.CursorRowResize}.Add(gtx.Ops)
+	}
+	m1.Pop()
+	m2.Pop()
 	c := m.Stop()
 	rs.Ratio = float32(rs.Pos) / float32(rs.Length)
 	return layout.Flex{
@@ -54,7 +91,7 @@ func (rs *Resize) Layout(gtx C, th *Theme, w1 layout.Widget, w2 layout.Widget) D
 		layout.Flexed(rs.Ratio, w1),
 		layout.Rigid(func(gtx C) D {
 			c.Add(gtx.Ops)
-			return dims
+			return D{Size: dims}
 		}),
 		layout.Flexed(1-rs.Ratio, w2),
 	)
